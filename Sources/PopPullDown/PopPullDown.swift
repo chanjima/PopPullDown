2439924d6c5a91415378f919ba013170eab5bc4f
chanjima <chanjimasuke@gmail.com>
2022-12-17 19:16:38 +0900

    ボタンの見た目を追加

--- Sources/PopPullDown/PopPullDown.swift
+++ Sources/PopPullDown/PopPullDown.swift
@@ -4,10 +4,25 @@ class PopPullDown: UIButton {
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
+        self.setupDegign()
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setupDegign()
     }
 
+}
+
+// MARK: - Private
+
+extension PopPullDown {
+
+    private func setupDegign() {
+        self.backgroundColor = .lightGray.withAlphaComponent(0.5)
+        self.setTitleColor(.label, for: .normal)
+        self.layer.cornerRadius = 10
+        self.layer.borderColor = UIColor.label.cgColor
+        self.layer.borderWidth = 1.0
+    }
 }
