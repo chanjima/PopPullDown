cb81f788a86289ecff45b551a612f003f5b415e2
chanjima <chanjimasuke@gmail.com>
2022-12-17 18:56:49 +0900

    初期テストコード削除

--- Tests/PopPullDownTests/PopPullDownTests.swift
+++ Tests/PopPullDownTests/PopPullDownTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import PopPullDown
 
 final class PopPullDownTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PopPullDown().text, "Hello, World!")
-    }
 }
